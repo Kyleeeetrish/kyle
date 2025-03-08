23f14bf20f61693715bb85541292670631580ff1
Kyleeeetrish <kylemacauana@gmail.com>
2025-03-09 00:17:48 +0800

    Add files via upload

---
+++ app.js.js
@@ -0,0 +1,35 @@
+// Example of the Data Bank (simulated with an array of objects)
+const dataBank = [
+    {
+        partsCode: "123456789",
+        partsName: "Printer Drum",
+        lineStation: "IPDB13-001",
+        status: "WAIT",
+        rfid: "ABC1234567",
+        reference: "1A2345678",
+        lotNumber: "1234567890123456",
+        remarks: "N/A",
+        creationTime: "2025-03-05 10:00:00",
+        pickTime: "2025-03-05 12:00:00",
+        transferTime: "2025-03-05 15:00:00",
+        deliveryTime: "2025-03-06 08:00:00",
+        userName: "Kyle Razsile Macauba"
+    },
+    {
+        partsCode: "987654321",
+        partsName: "Toner Cartridge",
+        lineStation: "IPDB13-002",
+        status: "TRANSFER",
+        rfid: "DEF2345678",
+        reference: "2B3456789",
+        lotNumber: "9876543210987654",
+        remarks: "N/A",
+        creationTime: "2025-03-06 09:00:00",
+        pickTime: "2025-03-06 11:00:00",
+        transferTime: "2025-03-06 13:00:00",
+        deliveryTime: "2025-03-07 09:00:00",
+        userName: "Kyle Razsile Macauba"
+    }
+    // Add as many entries as you need (you can create 30 or more as per your requirement)
+];
+
